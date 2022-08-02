b265adb8c1cf9d958be197add4980d7dbee7b8f3
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-08-02 20:19:38 +0530

    Create interface.java
    Interface are the blue print of the class. /it specify what a lass must do, and not how.

---
+++ java/interface.java
@@ -0,0 +1,23 @@
+interface I1
+{
+
+    public void show();
+
+}
+
+class test implements I1
+{
+
+public void show()
+{
+System.out.println("hiii vaidu");
+
+}
+
+public static void main(String args[])
+{
+test t=new test();
+t.show();
+
+}
+}Interface are the blue

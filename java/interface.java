09e44b5043fb5e2d3218c06d7e3bac1228b7ddf0
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-08-02 20:33:28 +0530

    Update interface.java

--- java/interface.java
+++ java/interface.java
@@ -1,8 +1,18 @@
 interface I1
 {
-
+    public static final int a=10; //all variables are must be public static final
     public void show();
+    default void display()   
+    {
+        System.out.println("hiii ");
+  
+    }
+                                               //we can only make default types method and static types methods                 
+    static void put()
+    {
+        System.out.println("hello");
 
+    }
 }
 
 class test implements I1
@@ -18,6 +28,9 @@ public static void main(String args[])
 {
 test t=new test();
 t.show();
+t.display();
+
+
 
 }
-}Interface are the blue
+}

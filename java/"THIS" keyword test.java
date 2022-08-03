4c1fea19def293be51186455cea4e29c1539693f
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-08-03 20:19:56 +0530

    Update "THIS" keyword test.java
    "this" keyword is the reference variable that refers to the current object.

--- java/"THIS" keyword test.java
+++ java/"THIS" keyword test.java
@@ -1,10 +1,13 @@
+/* code with out "this" key-word */ 
 class test
 {
 int i;
-void setValue(int x)
+void setValue(int i)
 {
-i=x;
-
+i=i;								/* with out "this" key-word, because of two "i" variable, 
+								one is globle and one is local, 								
+								compiler give the default value of variable . 								
+								for int default value is 0. */   
 }
 
 void show()
@@ -12,6 +15,35 @@ void show()
 	System.out.println(i);
 }
 }
+class xyz
+{
+public static void main(String args[])
+{
+	test t=new test();
+	t.setValue(10);
+	t.show();
+
+}
+
+
+}
+
+
+/* program with "this" key-word */
+class test
+{
+int i;
+void setValue(int i)
+{
+this.i=i;
+
+}
+
+void show()
+{
+	System.out.println(i);
+}
+} 
 class xyz
 {
 public static void main(String args[])

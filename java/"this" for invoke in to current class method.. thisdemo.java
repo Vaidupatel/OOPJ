af9935c1881a793fab5ef63fe0e7f30637562f16
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-08-05 19:07:49 +0530

    Update "this" for invoke in to current class method.. thisdemo.java

--- java/"this" for invoke in to current class method.. thisdemo.java
+++ java/"this" for invoke in to current class method.. thisdemo.java
@@ -6,7 +6,7 @@ void display()
 }
 void show()
 {
-	this.display();// display();   // both are same
+	this.display();// display();   // it's print "hello bro!!" because of "this" keyword, if we don't write this keyword than compiler assume that it self that ther is elrady this keyword.
 }
 public static void main(String args[])
 {

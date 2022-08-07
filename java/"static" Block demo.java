4b1c2f5e3e5b8b0a38da6b8d3be640c4e038e1a8
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-08-07 19:51:32 +0530

    Update "static" Block demo.java

--- java/"static" Block demo.java
+++ java/"static" Block demo.java
@@ -13,3 +13,9 @@ class demo {
         System.out.println("2nd static block");
     }
 }
+
+
+/*Static block executes automatically when class loaded in the memory.
+Static block execute first and than main method however the main method written first or after the main method.
+We can create multiple static block, at that time order of static block is top to bottom and than main method will be execute. 
+*/

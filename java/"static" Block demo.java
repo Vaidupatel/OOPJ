fe1a852d08fc77a0d65ba7c9c4ddfb5aa2caa339
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-08-07 19:33:04 +0530

    Create "static" Block demo.java
    Static method executes automatically when the class loaded in the memory.
    Static block executes first and then main method, How ever the main method is written first or after static block.

---
+++ java/"static" Block demo.java
@@ -0,0 +1,12 @@
+/*Ststic block executes automatically*/
+class demo {
+
+    public static void main(String[] args) {
+        System.out.println("i am in main method");
+
+    }
+
+    static {
+        System.out.println("hello world!");
+    }
+}
